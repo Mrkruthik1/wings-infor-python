f786fe71ffd129cffc237ded9897b99c01b69ab4
Mrkruthik1 <jkruthik344@gmail.com>
2026-09-08 18:02:23 +0530

    updated code for encapsulation

--- encapsulations.py
+++ encapsulations.py
@@ -4,7 +4,46 @@ class bank:
     def __init__(self,name,accountpass):
        self.name=name
        self.__accountpass=accountpass
+    def get__accountpass(self):
+        return self.__accountpass
 
 b=bank("kruthik",1234)
 print(b.name)
-print(b.__accountpass) # this is private " double underscore (__variable)"
+"""print(b.__accountpass)   this is private " double underscore (__variable)"   """
+
+print("-------------------------------")
+class bank:
+    def __init__(self,name,accountpass):
+       self.name=name
+       self.__accountpass=accountpass
+    def get__accountpass(self):
+        return self.__accountpass
+b=bank("k",101)
+print(b.get__accountpass()) 
+
+
+print("-------------------------------")
+
+class account:
+    def __init__(self, name, password):
+        self.name = name
+        self.__password = password
+
+    def get__password(self):
+        return self.__password
+
+    def set__password(self, password):
+        if password > 0:
+            self.__password = password
+        else:
+            print("pass should be positive")
+
+
+a = account("modi", -1)
+
+print(a.get__password())   # -1
+
+a.set__password(1)
+
+print(a.get__password())   # 1
+
