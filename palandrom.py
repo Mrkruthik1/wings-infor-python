10a25878f748aee3eb5580448085987b677405e8
Mrkruthik1 <jkruthik344@gmail.com>
2026-09-10 09:55:24 +0530

    palandrom

--- palandrom.py
+++ palandrom.py
@@ -1,17 +1,27 @@
-n=141
-nk=str(n)
-print(nk[::-1])
+# n=141
+# nk=str(n)
+# print(nk[::-1])
 
 
+# s=int(input("enter numbers :"))
+# s=str(s)
+# if s==s[::-1]:
+#     print("palandrom")
+# else:
+#     print("not a palandrom")
 
 
+# print("-------------------------")
 
 
-s=int(input("enter numbers :"))
+s="iaia"
+rev=""
 s=str(s)
-if s==s[::-1]:
+
+for i in s:
+    rev=i+rev
+if s==rev:
     print("palandrom")
 else:
     print("not a palandrom")
-
 
